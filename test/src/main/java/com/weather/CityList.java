be680c1d8dc508a243390e11d7b780087f03552c
Alfonso Cruz <alfonso.cruz@staples.com>
2016-02-20 19:46:26 -0800

    Added CityList test by city Ids, Rectangle, Group

--- test/src/main/java/com/weather/CityList.java
+++ test/src/main/java/com/weather/CityList.java
@@ -3,12 +3,64 @@ package com.weather;
 import java.io.Serializable;
 import java.util.List;
 
-public class CityList implements Serializable{
+public class CityList implements Serializable {
 	/**
 	 * 
 	 */
-    private static final long serialVersionUID = 1L;
-	private List<City> city;
-	
-	
+	private static final long serialVersionUID = 1L;
+	private String cod;
+	private String calctime;
+	private String cnt;
+	private String message;
+	private String count;
+	private List<City> list;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getCount() {
+		return count;
+	}
+
+	public void setCount(String count) {
+		this.count = count;
+	}
+
+	public String getCod() {
+		return cod;
+	}
+
+	public void setCod(String cod) {
+		this.cod = cod;
+	}
+
+	public String getCalctime() {
+		return calctime;
+	}
+
+	public void setCalctime(String calctime) {
+		this.calctime = calctime;
+	}
+
+	public String getCnt() {
+		return cnt;
+	}
+
+	public void setCnt(String cnt) {
+		this.cnt = cnt;
+	}
+
+	public List<City> getList() {
+		return list;
+	}
+
+	public void setList(List<City> list) {
+		this.list = list;
+	}
+
 }
--- test/src/test/java/com/weather/TestApplicationTests.java
+++ test/src/test/java/com/weather/TestApplicationTests.java
@@ -4,12 +4,16 @@
 package com.weather;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Spliterator;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.internal.util.collections.ArrayUtils;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.HttpEntity;
@@ -42,11 +46,18 @@ public class TestApplicationTests {
 	/** The url. */
 	final String url = "http://api.openweathermap.org/data/2.5/forecast/city";
 	
+	final String baseUrl = "http://api.openweathermap.org/data/2.5/";
 	/** The url city. */
-	final String urlCity = "http://api.openweathermap.org/data/2.5/weather";
+	final String urlCity = baseUrl+"weather";
+	
+	final String urlBox = baseUrl+"box/city";
+	
+	final String urlFind = baseUrl+"find";
+	
+	final String urlGroup = baseUrl+"group";
 	
 	/** The app id. */
-	final String appId = "X";
+	final String appId = "443d426df86b1a15193ae59b476450f8";
 	
 	/** The city. */
 	private String city = "524901";
@@ -115,18 +126,18 @@ public class TestApplicationTests {
 			City cityById = testCityWeatherById(city.getId());
 			Assert.notNull(cityById, "Testing by Id Failed: The city should not be null: "+city.getId());
 			Assert.notNull(cityById.getSys().getCountry(), "Testing by id Failed: The country should not be null: "+city.getId());
-			System.out.println(cityById.getName());
-			System.out.println(cityById.getSys().getCountry());
-			System.out.println(cityById.getCod());
+			//System.out.println(cityById.getName());
+			//System.out.println(cityById.getSys().getCountry());
+			//System.out.println(cityById.getCod());
 			
 			City cityByGeo = testCityByGeo(cityById.getCoord().getLat(), cityById.getCoord().getLon());
 			Assert.notNull(cityByGeo, "Testing by Geo Failed: The city should not be null: "+"Lat: " +cityById.getCoord().getLat()+ "Lon: " + cityById.getCoord().getLon());
 			Assert.notNull(cityByGeo.getSys().getCountry(), "Testing by Geo Failed: The country should not be null: "+"Lat: " +cityById.getCoord().getLat()+ "Lon: " + cityById.getCoord().getLon());
-			System.out.println(cityByGeo.getName());
-			System.out.println(cityByGeo.getSys().getCountry());
-			System.out.println(cityByGeo.getCod());
+			//System.out.println(cityByGeo.getName());
+			//System.out.println(cityByGeo.getSys().getCountry());
+			//System.out.println(cityByGeo.getCod());
 		}
-		System.out.println("Tested: " + cities.length +" cities: " + cities);
+		System.out.println("Tested: " + cities.length +" cities: ");
 	}
 	
 	/**
@@ -153,6 +164,99 @@ public class TestApplicationTests {
 		System.out.println("Tested: " + zipCodes.length + " zip codes.");
 	}
 	
+	@Test
+	public void testWheaterByRectangleBbox() throws JsonParseException, JsonMappingException, IOException{
+		String bbox = "12,32,15,37,10";
+		String cluster = "yes";
+
+		CityList cityList = this.testCityWeatherByRectangle(bbox, cluster);
+		Assert.notNull(cityList, "The cityList should not be null");
+		Assert.notNull(cityList.getCod(), "The cityList.cod should not be null");
+		Assert.notNull(cityList.getCalctime(), "The cityList.calctime should not be null");
+		for(City city : cityList.getList()){
+			Assert.notNull(city, "The city should not be null");
+			Assert.notNull(city.getName(), "The city.name should not be null");
+			Assert.notNull(city.getMain().getTemp_min(), "The city.temp_min should not be null");
+			Assert.notNull(city.getMain().getTemp_max(), "The city.temp_min should not be null");
+			Assert.notNull(city.getMain().getPressure(), "The city.pressure should not be null");
+			Assert.notNull(city.getWeather(), "The city.weather should not be null");
+			for(Weather weather : city.getWeather()){
+				Assert.notNull(weather.getMain(), "The city.weather.main should not be null");
+				Assert.notNull(weather.getDescription(), "The city.weather.description should not be null");
+				System.out.println(weather.getMain() + " " + weather.getDescription());
+			}
+			System.out.println(city.getName());
+			//System.out.println(city.getSys().getCountry());
+			System.out.println(city.getCod());
+		}
+
+		System.out.println("Tested: " + cityList.getList().size() + " cities.");
+	}
+	
+	@Test
+	public void testWheaterByCycle() throws JsonParseException, JsonMappingException, IOException{
+		String lat = "55.5";
+		String lon = "37.5";
+		String cnt = "10";
+
+		CityList cityList = this.testCityWeatherByCycle(lat, lon, cnt);
+		Assert.notNull(cityList, "The cityList should not be null");
+		Assert.notNull(cityList.getMessage(), "The cityList.message should not be null");
+		Assert.notNull(cityList.getCod(), "The cityList.cod should not be null");
+		Assert.notNull(cityList.getCount(), "The cityList.count should not be null");
+		for(City city : cityList.getList()){
+			Assert.notNull(city, "The city should not be null");
+			Assert.notNull(city.getName(), "The city.name should not be null");
+			Assert.notNull(city.getMain().getTemp_min(), "The city.temp_min should not be null");
+			Assert.notNull(city.getMain().getTemp_max(), "The city.temp_min should not be null");
+			Assert.notNull(city.getMain().getPressure(), "The city.pressure should not be null");
+			Assert.notNull(city.getWeather(), "The city.weather should not be null");
+			for(Weather weather : city.getWeather()){
+				Assert.notNull(weather.getMain(), "The city.weather.main should not be null");
+				Assert.notNull(weather.getDescription(), "The city.weather.description should not be null");
+				System.out.println(weather.getMain() + " " + weather.getDescription());
+			}
+			System.out.println(city.getName());
+			//System.out.println(city.getSys().getCountry());
+			System.out.println(city.getCod());
+		}
+
+		System.out.println("Tested: " + cityList.getList().size() + " cities.");
+	}
+	
+	@Test
+	public void testWheaterByGroup() throws JsonParseException, JsonMappingException, IOException{
+		String citiesIds = new String();
+		for(String cityId : cityIds){
+			if(citiesIds.isEmpty()){
+				citiesIds+=cityId;
+			}else{
+				citiesIds+=","+cityId;
+			}
+		}
+		String units = "metric";
+		CityList cityList = this.testCityWeatherByGroup(citiesIds, units);
+		Assert.notNull(cityList, "The cityList should not be null");
+		Assert.notNull(cityList.getCnt(), "The cityList.cnt should not be null");
+		for(City city : cityList.getList()){
+			Assert.notNull(city, "The city should not be null");
+			Assert.notNull(city.getName(), "The city.name should not be null");
+			Assert.notNull(city.getMain().getTemp_min(), "The city.temp_min should not be null");
+			Assert.notNull(city.getMain().getTemp_max(), "The city.temp_min should not be null");
+			Assert.notNull(city.getMain().getPressure(), "The city.pressure should not be null");
+			Assert.notNull(city.getWeather(), "The city.weather should not be null");
+			for(Weather weather : city.getWeather()){
+				Assert.notNull(weather.getMain(), "The city.weather.main should not be null");
+				Assert.notNull(weather.getDescription(), "The city.weather.description should not be null");
+				System.out.println(weather.getMain() + " " + weather.getDescription());
+			}
+			System.out.println(city.getName());
+			//System.out.println(city.getSys().getCountry());
+		}
+
+		System.out.println("Tested: " + cityList.getList().size() + " cities by group");
+	}
+	
 	/**
 	 * Test city weather by zip.
 	 *
@@ -169,7 +273,7 @@ public class TestApplicationTests {
 	 *             Signals that an I/O exception has occurred.
 	 */
 	private City testCityWeatherByZip(String zipCode, String country) throws JsonParseException, JsonMappingException, IOException{
-		return executeExchange("zip="+zipCode+","+country);
+		return executeExchange(urlCity,"zip="+zipCode+","+country);
 	}
 	
 	/**
@@ -186,7 +290,7 @@ public class TestApplicationTests {
 	 *             Signals that an I/O exception has occurred.
 	 */
 	private City testCityWeather(String city) throws JsonParseException, JsonMappingException, IOException{
-		return executeExchange("q="+city);
+		return executeExchange(urlCity,"q="+city);
 	}
 	
 	/**
@@ -203,7 +307,7 @@ public class TestApplicationTests {
 	 *             Signals that an I/O exception has occurred.
 	 */
 	private City testCityWeatherById(String cityId) throws JsonParseException, JsonMappingException, IOException{
-		return executeExchange("id="+cityId);
+		return executeExchange(urlCity,"id="+cityId);
 	}
 	
 	/**
@@ -222,7 +326,19 @@ public class TestApplicationTests {
 	 *             Signals that an I/O exception has occurred.
 	 */
 	private City testCityByGeo(String lat, String lon) throws JsonParseException, JsonMappingException, IOException{
-		return executeExchange("lat="+lat+"&lon="+lon);
+		return executeExchange(urlCity,"lat="+lat+"&lon="+lon);
+	}
+	
+	private CityList testCityWeatherByRectangle(String bbox, String cluster) throws JsonParseException, JsonMappingException, IOException{
+		return executeExchangeList(urlBox,"bbox="+bbox+"&cluster="+cluster);
+	}
+	
+	private CityList testCityWeatherByCycle(String lat, String lon, String cnt) throws JsonParseException, JsonMappingException, IOException{
+		return executeExchangeList(urlFind,"lat="+lat+"&lon="+lon+"&cnt="+cnt);
+	}
+	
+	private CityList testCityWeatherByGroup(String cities,String units) throws JsonParseException, JsonMappingException, IOException{
+		return executeExchangeList(urlGroup,"id="+cities + "&units="+units);
 	}
 	
 	/**
@@ -238,13 +354,22 @@ public class TestApplicationTests {
 	 * @throws IOException
 	 *             Signals that an I/O exception has occurred.
 	 */
-	private City executeExchange(String parameters) throws JsonParseException, JsonMappingException, IOException{
+	private City executeExchange(String urlParam, String parameters) throws JsonParseException, JsonMappingException, IOException{
 		restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
 		HttpHeaders requestHeaders = new HttpHeaders();
 		HttpEntity<?> requestEntity = new HttpEntity<Object>(requestHeaders);
-		ResponseEntity<String> response = restTemplate.exchange(urlCity+"?"+parameters+"&appid="+appId, HttpMethod.GET, requestEntity,  String.class);
+		ResponseEntity<String> response = restTemplate.exchange(urlParam+"?"+parameters+"&appid="+appId, HttpMethod.GET, requestEntity,  String.class);
 		System.out.println(response.getBody());
 		return new Gson().fromJson(response.getBody(), City.class);  
+	}
+	
+	private CityList executeExchangeList(String urlParam, String parameters) throws JsonParseException, JsonMappingException, IOException{
+		restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+		HttpHeaders requestHeaders = new HttpHeaders();
+		HttpEntity<?> requestEntity = new HttpEntity<Object>(requestHeaders);
+		ResponseEntity<String> response = restTemplate.exchange(urlParam+"?"+parameters+"&appid="+appId, HttpMethod.GET, requestEntity,  String.class);
+		System.out.println(response.getBody());
+		return new Gson().fromJson(response.getBody(), CityList.class);  
 	}
 	
 	/** The cities. */
@@ -289,4 +414,8 @@ public class TestApplicationTests {
 			"30315",
 			"30318",
 			"30319"};
+	
+	private String[] cityIds = {"384848","475279","4344544","6199126",
+			"3007202","3333225","211098","6553056","2916630","3495858","6356141","6454050","6454341","6455404","7298888",
+			"7300545","612366","3716661","6537554","6541492","4012176","4006702"};
 }
